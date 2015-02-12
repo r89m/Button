413f774824b086a4a5466d2284c392be5f06bb9f
Richard Miles <richard.miles@fast-chat.co.uk>
2015-02-12 17:48:22 +0000

    Changed some PushButton example wording

--- PushButton/examples/PushButton/PushButton.ino
+++ PushButton/examples/PushButton/PushButton.ino
@@ -32,7 +32,7 @@ void loop(){
 }
 
 // Use this function to configure the internal Bounce object to suit you. See the documentation at: https://github.com/thomasfredericks/Bounce2/wiki
-// This function can be left empty if the defaults are acceptable
+// This function can be left out if the defaults are acceptable - just don't call configureButton
 void configurePushButton(Bounce& bouncedButton){
     
         // Set the debounce interval to 15ms - default is 10ms
