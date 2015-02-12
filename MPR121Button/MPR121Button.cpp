10af6bc387af9a79c9268ba24a20bbceca52e4c7
Richard Miles <richard.miles@fast-chat.co.uk>
2015-02-12 12:36:25 +0000

    Implement MPR121Button Class
    Powered by on Adafruit's MPR121 library

---
+++ MPR121Button/MPR121Button.cpp
@@ -0,0 +1,38 @@
+/*
+ * CapacitiveButton.cpp
+ *
+ * Created: 18/11/2014 19:33:23
+ *  Author: Richard
+ */ 
+
+#include "MPR121Button.h"
+
+MPR121Button::MPR121Button(Adafruit_MPR121 &touchSensor, uint8_t electrode){
+	
+	_touchSensor = &touchSensor;
+	_electrode = electrode;
+}
+
+void MPR121Button::update(uint16_t latestReading){
+	
+	_latestReading = latestReading;
+	Button::update();
+}
+
+boolean MPR121Button::_update_button_state(){
+	
+	uint16_t touchSwitchState;
+	
+	// Check to see if we were given a reading from the touch sensor (this will help minimise the requests over i2c)
+	if(_latestReading){
+		touchSwitchState = _latestReading;
+		// Reset the latest reading variable to zero. This ensures that we don't hang on to an old value
+		_latestReading = 0;
+	} else {		
+		// Read the latest value from the touch switch
+		touchSwitchState = _touchSensor->touched();
+	}
+	
+	// Return whether it is pressed or not
+	return (bitRead(touchSwitchState, _electrode));
+}
---
+++ MPR121Button/MPR121Button.h
@@ -0,0 +1,30 @@
+/*
+ * CapacitiveButton.h
+ *
+ * Created: 18/11/2014 19:34:16
+ *  Author: Richard
+ */ 
+
+
+#ifndef MPR121BUTTON_H_
+#define MPR121BUTTON_H_
+
+#include "Button.h";
+#include <Adafruit_MPR121.h>
+
+class MPR121Button : public Button {
+	
+	private:
+		Adafruit_MPR121* _touchSensor;			// Store a reference to an MPR121 Touch Sensor
+		uint8_t _electrode;						// Which electrode are we monitoring
+		uint16_t _latestReading = 0;			// The latest reading from the touch sensor. This helps minimise requests over the i2c bus
+	
+	protected:
+		boolean _update_button_state();
+	
+	public:
+		MPR121Button(Adafruit_MPR121&, uint8_t);	
+		void update(uint16_t);
+};
+
+#endif /* MPR121BUTTON_H_ */
