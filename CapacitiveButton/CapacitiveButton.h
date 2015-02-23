5e4cfcedb48dd15dc304909f421f36ddf86a85d0
Richard Miles <richard.miles@fast-chat.co.uk>
2015-02-23 21:56:39 +0000

    Remove NOT_A_PIN dependency
    NOT_A_PIN is not defined with Teensy 3.1 - used -1 instead

--- CapacitiveButton/CapacitiveButton.h
+++ CapacitiveButton/CapacitiveButton.h
@@ -9,7 +9,7 @@
 #ifndef CAPACITIVEBUTTON_H_
 #define CAPACITIVEBUTTON_H_
 
-#include "Button.h";
+#include "Button.h"
 #include "CapacitiveSensor.h"
 
 // Define button configuration callback
@@ -18,7 +18,7 @@ typedef void (*CapacitiveButtonConfigurationCallback)(CapacitiveSensor&);
 class CapacitiveButton : public Button {
 	
 	private:
-	CapacitiveSensor capSense = CapacitiveSensor(NOT_A_PIN, NOT_A_PIN);	// Called with pins set to NOT_A_PIN as there is not default constructor
+	CapacitiveSensor capSense = CapacitiveSensor(-1, -1);	// Called with pins set to -1 as there is not default constructor
 	long _threshold = 1000;
 	long _number_of_samples = 30;
 	
